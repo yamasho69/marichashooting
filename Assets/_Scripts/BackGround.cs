5c6673fff9a522bce6261fc0508c3f4f3f0964a1
yamasho69 <nitiham55@yahoo.co.jp>
2022-06-02 00:15:26 +0900

    6回目のコミット
    背景のスクロール完成

--- Assets/_Scripts/BackGround.cs
+++ Assets/_Scripts/BackGround.cs
@@ -1,17 +1,20 @@
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-//using DG.Tweening;
-
+using UnityEngine;
+ 
 public class BackGround : MonoBehaviour {
-    public float scrollSpeed;
+    //スクロールスピード
+    [SerializeField] float speed = 1;
+    [SerializeField] float limitPosition;
+    [SerializeField] float loopPosition;
 
-    private void Update() {
-        Vector2 offset = new Vector2(0, Mathf.Repeat(Time.time * scrollSpeed, 1f));
-        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex",offset);
+    void Update() {
+        //下方向にスクロール
+        transform.position -= new Vector3(0, Time.deltaTime * speed);
+
+        //Yが-11まで来れば、yPositionまで移動する
+        if (transform.position.y <= limitPosition) {
+            transform.position = new Vector2(0, loopPosition);
+        }
     }
 }
